389c50ea471e7b79edf91d4bca24d4b4ba5f7155
Charles Xavier <charlesxsaldanha@hotmail.com>
2021-11-28 15:23:48 -0300

    exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script3.js
@@ -0,0 +1,11 @@
+const a = 17;
+const b = 77;
+const c = 27;
+
+if (a > b && a > c) {
+  console.log('O número maior é ' + a);
+} else if (b > a && b > c) {
+  console.log('O número maior é ' + b);
+} else {
+  console.log('O número maior é ' + c);
+};
